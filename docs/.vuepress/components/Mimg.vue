254f794b48054a040e0d2bcd62024d68b3795646
DESKTOP-3VSNS1I\leon <501646203@qq.com>
2022-11-24 11:28:32 +0800

    打包 $withBase

--- docs/.vuepress/components/Mimg.vue
+++ docs/.vuepress/components/Mimg.vue
@@ -1,10 +1,11 @@
 <template>
   <br>
-  <img alt="image.png" :src="`/images/${src}`" :width="width">
+  <img alt="image.png" :src="withBase(`/images/${src}`)" :width="width">
   <br>
 </template>
 
 <script setup>
+import { withBase } from '@vuepress/client'
 /**
  * @file 自定义图片组件添加上下换行和固定的图片目录
  */
